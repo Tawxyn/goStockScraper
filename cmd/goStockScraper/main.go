3b73165102ec82020161c250e19fea012b38deee
Tawxyn <oleshchukmike@gmail.com>
2024-05-11 15:23:30 -0700

    Item struct working as intended (Still need to provide JSON exit). onHTML now pulled correct cash-flow values for the 4 years avaliable.

--- cmd/goStockScraper/main.go
+++ cmd/goStockScraper/main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/gocolly/colly"
 )
 
-type Item struct {
+// Json item structure for scalability / orginization
+type item struct {
 	FCF_Year1 string `json:"FCF1"`
 	FCF_Year2 string `json:"FCF2"`
 	FCF_Year3 string `json:"FCF3"`
@@ -26,7 +27,7 @@ func main() {
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 	// Item Truct slice
-	items := []Item{}
+	items := []item{}
 	// Prior vist, request
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
@@ -35,22 +36,25 @@ func main() {
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
+	// Confirm Response
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
 	})
-	// Scrape FCF
-	c.OnHTML("div.tableBody div.", func(e *colly.HTMLElement) {
-		text := e.Text
-		i := Item{
-			FCF_Year1: text,
+	// Scrape FCF, goes to last row of table body div, and selects chosen childs
+	c.OnHTML("div.tableBody div.row:last-of-type", func(e *colly.HTMLElement) {
+		item := item{
+			FCF_Year1: e.ChildText("div:nth-child(3)"),
+			FCF_Year2: e.ChildText("div:nth-child(4)"),
+			FCF_Year3: e.ChildText("div:nth-child(5)"),
+			FCF_Year4: e.ChildText("div:nth-child(6)"),
 		}
-		items = append(items, i)
+		items = append(items, item)
 	})
-
+	// Confirmed vist and done filling out OnHTML callback
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 	})
-
+	// URL setup from User input
 	url := fmt.Sprintf("https://finance.yahoo.com/quote/%s/cash-flow", ticker)
 	c.Visit(url)
 	fmt.Println(items)
