b2df2c6ccdab5d5d6db0bf2750cbaff9e4b8375c
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2024-05-28 23:27:57 -0700

    Import Functional
    Got database package to finally import, set ground work for inserting data for FCF years. Had to use string int conversion in database. Code terminates successfully but PGadm4 is not displaying anything being added to table despite getting commits and transactions on dashboard.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	database "github.com/Tawxyn/goStockScraper/pkg"
 	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 )
 
 // Json item structure for scalability / orginization
-type item struct {
+type newItem struct {
 	FCF_Year1 string `json:"FCF1"`
 	FCF_Year2 string `json:"FCF2"`
 	FCF_Year3 string `json:"FCF3"`
@@ -17,11 +19,25 @@ type item struct {
 }
 
 func main() {
+
 	//Load environment variables from .env file
-	err := godotenv.Load("../.env")
+	err := godotenv.Load("../../.env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Fatalf("Error loading .env file: %v\n", err)
 	}
+
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		log.Fatalf("DATABASE_URL was not found in the .env or is empty")
+	}
+	fmt.Println(connString)
+
+	// Database import (pkg/database.go)
+	err = database.InitDatabase(connString)
+	if err != nil {
+		log.Fatalf("Error in initializing database post .env load: %v\n", err)
+	}
+	defer database.Close() // Close database after main exists
 
 	ticker := tickerInput()
 	// Initiate new collector
@@ -34,7 +50,7 @@ func main() {
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 	// Item sruct slice
-	items := []item{}
+	var items []newItem
 	// Prior vist, request
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
@@ -49,13 +65,13 @@ func main() {
 	})
 	// Scrape FCF, goes to last row of table body div, and selects chosen childs
 	c.OnHTML("div.tableBody div.row:last-of-type", func(e *colly.HTMLElement) {
-		item := item{
+		newItem := newItem{
 			FCF_Year1: e.ChildText("div:nth-child(3)"),
 			FCF_Year2: e.ChildText("div:nth-child(4)"),
 			FCF_Year3: e.ChildText("div:nth-child(5)"),
 			FCF_Year4: e.ChildText("div:nth-child(6)"),
 		}
-		items = append(items, item)
+		items = append(items, newItem)
 	})
 	// Confirmed vist and done filling out OnHTML callback
 	c.OnScraped(func(r *colly.Response) {
@@ -65,6 +81,14 @@ func main() {
 	url := fmt.Sprintf("https://finance.yahoo.com/quote/%s/cash-flow", ticker)
 	c.Visit(url)
 	fmt.Println(items)
+
+	database.InsertFCF(ticker, "bruh", items[0].FCF_Year2, items[0].FCF_Year3, items[0].FCF_Year4)
+	if err != nil {
+		fmt.Printf("Error insertintg data into database: %v\n", err)
+	} else {
+		fmt.Println("Data insert succesffuly")
+	}
+
 }
 
 // Obtain user ticker info.
--- pkg/database.go
+++ pkg/database.go
@@ -2,25 +2,63 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// Create a single instance
 var pool *pgxpool.Pool
 
+// Initalize the database with pgxpool
 func InitDatabase(connString string) error {
 	var err error
 	pool, err = pgxpool.New(context.Background(), connString)
 	return err
 }
 
-func InsertData(ticker string, cashFlow2020, cashFlow2021, cashFlow2022, cashFlow2023 int) error {
+// Close function to shutdown gracefully
+func Close() {
+	if pool != nil {
+		pool.Close()
+	}
+}
+
+// Insert cashflow values for 2020 - 2023 years
+func InsertFCF(ticker string, cashFlow2020, cashFlow2021, cashFlow2022, cashFlow2023 string) error {
+	var err error
+
+	// Convert string values to integers
+	cf2020, err := strconv.Atoi(cashFlow2020)
+	if err != nil {
+		return fmt.Errorf("failed to convert cashFlow2020 to integer: %v", err)
+	}
+
+	cf2021, err := strconv.Atoi(cashFlow2021)
+	if err != nil {
+		return fmt.Errorf("failed to convert cashFlow2021 to integer: %v", err)
+	}
+
+	cf2022, err := strconv.Atoi(cashFlow2022)
+	if err != nil {
+		return fmt.Errorf("failed to convert cashFlow2022 to integer: %v", err)
+	}
+
+	cf2023, err := strconv.Atoi(cashFlow2023)
+	if err != nil {
+		return fmt.Errorf("failed to convert cashFlow2023 to integer: %v", err)
+	}
 	query := `
         INSERT INTO stock_cash_flow 
             (ticker, cash_flow_2020, cash_flow_2021, cash_flow_2022, cash_flow_2023) 
         VALUES 
             ($1, $2, $3, $4, $5)`
 
-	_, err := pool.Exec(context.Background(), query, ticker, cashFlow2020, cashFlow2021, cashFlow2022, cashFlow2023)
-	return err
+	_, err = pool.Exec(context.Background(), query, ticker, cf2020, cf2021, cf2022, cf2023)
+	if err != nil {
+		return fmt.Errorf("failed to insert data into database: %v", err)
+	}
+
+	return nil
 }
